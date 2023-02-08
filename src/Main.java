9cac6b1083681f5af102efa2676df3cfe5691dc8
RenitaT <123038920+RenitaT@users.noreply.github.com>
2023-02-08 09:17:00 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+                Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the distance to travel in nautical miles:");
+
+        int distance = scanner.nextInt();
+
+        double regularMiles = distance * 1.15;
+
+        System.out.println("Enter the top speed of the boat in knots:");
+
+        double knots1 = scanner.nextDouble();
+
+        double best = distance / knots1;
+
+        System.out.println("Enter the average speed of the boat in knots:");
+
+        double knots2 = scanner.nextDouble();
+
+        double average = distance / knots2;
+
+        String output = "The distance to travel in regular miles is: " + regularMiles + " miles\n"+"The best case travel time is: " + best + " hours at " + knots1 + " knots\n" + "The average case travel time is: " + average + " hours at " + knots2 + " knots";
+
+        System.out.println(output);
     }
 }
